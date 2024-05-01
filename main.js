b67113edea82016eb49647ed4e6b402bf0d40e43
JDSerna00 <83099671+JDSerna00@users.noreply.github.com>
2024-05-01 12:57:10 -0500

    Building the display

--- main.js
+++ main.js
@@ -2,4 +2,15 @@ let cart = [];
 
 function addToCart(productName, price) {
   cart.push({ name: productName, price: price });
+  updateCartDisplay();
 }
+
+function updateCartDisplay() {
+    const cartItemsElement = document.getElementById('cart-items');
+    cartItemsElement.innerHTML = '';
+    cart.forEach(item => {
+      const li = document.createElement('li');
+      li.textContent = `${item.name} - $${item.price}`;
+      cartItemsElement.appendChild(li);
+    });
+  }
