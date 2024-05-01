07d2b71d22f47eaa29535d57e42514f5aab2909c
JDSerna00 <83099671+JDSerna00@users.noreply.github.com>
2024-05-01 13:50:26 -0500

    RemoveButton

--- main.js
+++ main.js
@@ -5,12 +5,21 @@ function addToCart(productName, price) {
   updateCartDisplay();
 }
 
+function removeItem(index) {
+    cart.splice(index, 1);
+    updateCartDisplay();
+  }
+
 function updateCartDisplay() {
     const cartItemsElement = document.getElementById('cart-items');
     cartItemsElement.innerHTML = '';
-    cart.forEach(item => {
+    cart.forEach((item, index) => {
       const li = document.createElement('li');
       li.textContent = `${item.name} - $${item.price}`;
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'Remove';
+      removeButton.onclick = () => removeItem(index);
+      li.appendChild(removeButton);
       cartItemsElement.appendChild(li);
     });
   }
